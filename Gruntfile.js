739447801b07854247753830540701ef189cdc13
Myles Chatman <myleschatman@Myless-MacBook-Pro.local>
2017-01-11 22:45:41 -0800

    #

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,8 +64,8 @@ module.exports = function (grunt) {
         files: [
           '<%= yeoman.app %>/{,*/}*.html',
           '.tmp/styles/{,*/}*.css',
-          '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-          '<%= yeoman.app %>/images/fonts/*.{png}'
+          // '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+          // '<%= yeoman.app %>/images/fonts/*.{png}'
         ]
       }
     },
